3c6d7f9159d4f84c6e7b9211ed16c0375a181abc
binduneerukonda <kancheti.bindu@gmail.com>
2019-04-22 12:06:29 +0530

    consumer-console

--- consumer-console.py
+++ consumer-console.py
@@ -1,36 +1,31 @@
-from confluent_kafka import Consumer, KafkaError
 from msgpack import unpackb
-from datetime import datetime as dtm
-import sys
+from confluent_kafka import Consumer
+
+c=Consumer({'bootstrap.servers': '10.0.0.10','group.id': 'accounts-data','auto.offset.reset': 'earliest'})
 
 def decode_dtm(obj):
     if '__datetime__' in obj:
         obj=dtm.strptime(obj["as_str"], "%Y%m%dT%H:%M:%S")
     return obj
 
-c=Consumer({
-    'bootstrap.servers': '10.0.0.10',
-    'group.id': 'accounts-console',
-    'auto.offset.reset': 'earliest'
-})
-
 c.subscribe(['topic-accounts-patch','topic-accounts-purge'])
+
+def validateMessage(msg):
+    pct=True
+    if msg is None:
+        pct=False
+    elif msg.error():
+        print('Error: {}'.format(msg.error()))
+        pct=False
+    return pct
 
 while True:
     msg=c.poll(1.0)
-    if msg is None:
-        continue
-    if msg.error():
-        print("Consumer error: {}".format(msg.error()))
-        continue
-    if msg.topic()=='topic-accounts-patch':
-        print('Patch document...')
+    packet=validateMessage(msg)
+    if packet:
+        unp=unpackb(msg.value(),object_hook=decode_dtm,raw=False)
+		print(unp)
     else:
-        print('Erasing document...')
-    print('Message: {}'.format(msg.value()) +', Message Size: '+ str(sys.getsizeof(msg.value())))
-    unp=unpackb(msg.value(),object_hook=decode_dtm,raw=False)
-    print(unp)
-    print('Size after Unpacking: ' +str(sys.getsizeof(unp)))
-    print(' ')
+        continue
 
 c.close()
