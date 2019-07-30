c83736c9bda14d35db02ef2021a94972a251350b
Raghu Neerukonda <Raghu.Neerukonda@inspiredelearning.com>
2019-07-30 17:45:23 +0530

    xpost-2

--- xpost.py
+++ xpost.py
@@ -2,7 +2,7 @@ import random as R
 import sys
 from datetime import datetime as dtm,timedelta as tdt
 from barnum import gen_data as bgdata
-from flask import jsonify
+import json
 
 debug=False
 if len(sys.argv)<2:
@@ -27,7 +27,7 @@ def batchPoster(n):
         ,"channel_partner":R.choice([True,False]),"onboard_type":R.choice(["custom","partner","direct"])
         ,"start_date":str(dtm.utcnow().date()-tdt(days=R.choice(range(10,1000))))}
         reqList.append(document)
-    print(jsonify(reqList))
+    print(json.dumps(reqList))
     return None
 
 batchPoster(N)
