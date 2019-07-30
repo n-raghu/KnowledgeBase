cfdb7b6692ba24ec174e756ba6d2228b11195fb0
Raghu Neerukonda <Raghu.Neerukonda@inspiredelearning.com>
2019-07-30 14:02:09 +0530

    eventifier fix-1

--- service.py
+++ service.py
@@ -1,4 +1,5 @@
-from resources import Flask,JWTManager,Api,AccountID,getPostAcc,getNewToken,cfg
+from resources import Flask,JWTManager,Api,
+from resources import AccountID,getPostAcc,getNewToken,cfg,GetEvents
 
 app=Flask(__name__)
 app.config['JWT_SECRET_KEY']=cfg['app']['key']
@@ -8,6 +9,6 @@ api=Api(app)
 api.add_resource(getNewToken,'/login')
 api.add_resource(getPostAcc,'/accounts')
 api.add_resource(AccountID,'/accounts/<accid>')
-api.add_resource(AccountID,'/events/<eid>')
+api.add_resource(GetEvents,'/events/<eid>')
 
 app.run(debug=cfg['app']['debug'],host=cfg['app']['host'],port=cfg['app']['port'])
