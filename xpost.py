1534ed665516f038097b719243bb648eec807bb0
Raghu Neerukonda <Raghu.Neerukonda@inspiredelearning.com>
2019-07-30 17:35:46 +0530

    xpost

---
+++ xpost.py
@@ -0,0 +1,49 @@
+import requests as req
+import random as R
+import sys
+from datetime import datetime as dtm,timedelta as tdt
+from uuid import uuid1 as uid
+from yaml import safe_load
+from requests_jwt import JWTAuth
+from barnum import gen_data as bgdata
+
+with open('app.yml') as ymlFile:
+    cfg=safe_load(ymlFile)
+
+access='http://172.16.1.164:39099/v1/login'
+headers={'content-type':'application/json'}
+
+debug=False
+if len(sys.argv)<2:
+    N=1
+else:
+    N=int(sys.argv[1])
+
+if N<1:
+    sys.exit(N)
+
+instancelist=['swiss','US Farm','EU Farm']
+deploy_mode=['Potter','IM']
+auth=[{'uid':'admin','pwd':'adminpassword'},{'uid':'eaeuser','pwd':'eaeuserpassword'}
+      ,{'uid':'raghu','pwd':'raghupassword'},{'uid':'yogesh','pwd':'yogeshpassword'}]
+
+def getToken():
+    thisAuth=R.choice(auth)
+    token=req.post(url=access,json=thisAuth,verify=False)
+    return token
+
+def batchPoster(n):
+    reqList=[]
+    for i in range(0,n):
+        document={"account_name":bgdata.create_company_name(),"instancecode":R.choice(instancelist)
+        ,"lms_custid":R.choice(range(69,69069)),"deploy_mode":R.choice(deploy_mode),"active":True
+        ,"account_flag":R.choice([True,False]),"eae_integration":R.choice([True,False])
+        ,"channel_partner":R.choice([True,False]),"onboard_type":R.choice(["custom","partner","direct"])
+        ,"start_date":str(dtm.utcnow().date()-tdt(days=R.choice(range(10,1000))))}
+        reqList.append(document)
+    t=getToken()
+    dataHeadR={'Content-Type':'application/json','Authorization':'Bearer {}'.format(t.json())}
+    print(reqList)
+    return None
+
+batchPoster(N)
