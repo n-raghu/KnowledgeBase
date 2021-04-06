775698da235a142bd1034264c90906aca7f4313e
raghu <raghu@LCHVWXVJ2.inspireme.local>
2021-04-06 18:25:32 +0530

    DataStruts

---
+++ python/datastruts.py
@@ -0,0 +1,67 @@
+
+"""
+Stack is a linear data structure and follow LIFO.
+Stack can be implemented in Python using Lists.
+"""
+
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+
+"""
+Queue is a linear data structure and follow FIFO.
+Queue can be implemented in Python using Lists.
+"""
+
+
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+
+"""
+Deque is a linear data structure and follows custom rules.
+Deque can be implemented in Python using Lists.
+"""
+
+
+class Deque:
+
+    def __init__(self):
+        self.items = []
+
+    def add_front(self, item):
+        self.items.insert(0, item)
+
+    def add_rear(self, item):
+        self.items.append(item)
+
+    def remove_front(self):
+        return self.items.pop(0)
+
+    def remove_rear(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
